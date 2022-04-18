64024e3bf28ea164ce0cfc739ddb9b7268c6f1c1
mys9ys <rjbexa@yandex.ru>
2022-04-18 17:42:58 +0300

    экипировка

--- index.php
+++ index.php
@@ -2,62 +2,54 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Facets of Fortune</title>
+    <link rel="stylesheet" href="/style.min.css">
 </head>
 <body>
 
 <div class="game_container">
-    <img class="hero" src="/img/hero.png" alt="">
-    <div class="hero_info_block">
-        <div class="hero_nickname">Mys9ys</div>
-        <div class="hero_lvl">lvl: 1</div>
-        <div class="hero_hp">10/10</div>
-        <div class="hero_xp">0</div>
+    <div class="header">
+        <img class="hero_img" src="/img/hero.png" alt="">
+        <div class="hero_info_block">
+            <div class="hero_nickname">Mys9ys</div>
+            <div class="hero_lvl">lvl: 1</div>
+            <div class="hero_hp">10/10</div>
+            <div class="hero_xp">0</div>
+        </div>
     </div>
+
+    <div class="equipment_block">
+        <div class="equipment_main">
+            <div class="equipment_first">
+                <div class="equipment_item5"></div>
+                <div class="equipment_item4"></div>
+            </div>
+            <div class="equipment_second">
+                <div class="equipment_item5"></div>
+                <div class="equipment_item4"></div>
+            </div>
+            <div class="equipment_third">
+                <div class="equipment_item3"></div>
+                <div class="equipment_item1"></div>
+                <div class="equipment_item1"></div>
+                <div class="equipment_item3"></div>
+            </div>
+        </div>
+        <div class="equipment_jewelry">
+            <div class="jewelry_item1"></div>
+            <div class="jewelry_item1"></div>
+            <div class="jewelry_item1"></div>
+            <div class="jewelry_item1"></div>
+            <div class="jewelry_item3"></div>
+            <div class="jewelry_item1"></div>
+            <div class="jewelry_item1"></div>
+        </div>
+    </div>
+
+
 </div>
 </body>
 </html>
 <style>
-    body{
-        background: #474C52;
-        margin: 0;
-    }
-    .game_container{
-        border-top: 1px solid transparent;
-        width: 1200px;
-        margin: auto;
-        display: flex;
-        flex-direction: row;
-        color: #A6B2AA;
-        background: rgba(33,33,33,0.4);
-        box-shadow: -4px 4px 2px -5px rgb(255 255 255 / 40%), inset -1px -1px 2px rgb(0 0 0 / 20%);
-        padding: 4px;
-        border-radius: 3px;
 
-    }
-    .hero{
-        width: 60px;
-    }
-    .hero_info_block{
-
-    }
-    .hero_nickname, .hero_lvl{
-        font-weight: 600;
-    }
-
-    .hero_hp{
-        width: 100px;
-        height: 10px;
-        font-size: 10px;
-        background: rosybrown;
-        color: rgb(255,255,255);
-        margin-bottom: 3px;
-    }
-    .hero_xp{
-        width: 100px;
-        height: 10px;
-        font-size: 10px;
-        background: cornflowerblue;
-        color: rgb(255,255,255);
-    }
 </style>
