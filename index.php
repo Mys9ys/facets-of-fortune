0f4011446e2d3558dc916b3f2c41523761a29ca2
mys9ys <rjbexa@yandex.ru>
2022-04-18 18:41:21 +0300

    экипировка + картинки

--- index.php
+++ index.php
@@ -21,27 +21,49 @@
     <div class="equipment_block">
         <div class="equipment_main">
             <div class="equipment_first">
-                <div class="equipment_item5"></div>
-                <div class="equipment_item4"></div>
+                <div class="equipment_item5 equipment_sword">
+                    <img class="equipment_item_img" src="/img/equip_temp/sword.png" alt="">
+                </div>
+                <div class="equipment_item4 equipment_shield">
+                    <img class="equipment_item_img" src="/img/equip_temp/shield.png" alt="">
+                </div>
             </div>
             <div class="equipment_second">
-                <div class="equipment_item5"></div>
-                <div class="equipment_item4"></div>
+                <div class="equipment_item5 equipment_armor">
+                    <img class="equipment_item_img" src="/img/equip_temp/armor.png" alt="">
+                </div>
+                <div class="equipment_item4 equipment_pants">
+                    <img class="equipment_item_img" src="/img/equip_temp/pants.png" alt="">
+                </div>
             </div>
             <div class="equipment_third">
-                <div class="equipment_item3"></div>
-                <div class="equipment_item1"></div>
-                <div class="equipment_item1"></div>
-                <div class="equipment_item3"></div>
+                <div class="equipment_item3 equipment_helmet">
+                    <img class="equipment_item_img" src="/img/equip_temp/helmet.png" alt="">
+                </div>
+                <div class="equipment_item1">
+                    <img class="equipment_item_img" src="/img/equip_temp/gloves.png" alt="">
+                </div>
+                <div class="equipment_item1">
+                    <img class="equipment_item_img" src="/img/equip_temp/belt.png" alt="">
+                </div>
+                <div class="equipment_item3">
+                    <img class="equipment_item_img" src="/img/equip_temp/boots.png" alt="">
+                </div>
             </div>
         </div>
         <div class="equipment_jewelry">
+            <div class="jewelry_item1">
+                <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
+            </div>
             <div class="jewelry_item1"></div>
             <div class="jewelry_item1"></div>
             <div class="jewelry_item1"></div>
-            <div class="jewelry_item1"></div>
-            <div class="jewelry_item3"></div>
-            <div class="jewelry_item1"></div>
+            <div class="jewelry_item3">
+                <img class="jewelry_item_img" src="/img/equip_temp/necklace.png" alt="">
+            </div>
+            <div class="jewelry_item1">
+                <img class="jewelry_item_img" src="/img/equip_temp/bracelet.png" alt="">
+            </div>
             <div class="jewelry_item1"></div>
         </div>
     </div>
