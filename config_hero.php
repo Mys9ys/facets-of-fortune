e31e8eb3afaea282b2675a7bc759ccf313a79d36
mys9ys <rjbexa@yandex.ru>
2022-04-19 12:53:23 +0300

    скилы + ювелирка

---
+++ config_hero.php
@@ -0,0 +1,22 @@
+<?php
+$arHero = [
+    'heroInfo' => [
+        'nickname' => 'Mys9ys',
+        'lvl' => 1,
+        'hp'=> 10,
+        'hp_now' => 10,
+        'xp'=> 0
+    ],
+    'heroSkills' => [
+        ['name'=>'Сила', 'index' => 'power', 'count' => 0],
+        ['name'=>'Магия', 'index' => 'magic', 'count' => 0],
+        ['name'=>'Инициатива', 'index' => 'initiative', 'count' => 0],
+    ],
+    'skill_up' => 1,
+    'heroProperty' => [
+        ['name'=>'Атака оружием', 'index' => 'attack_weapon', 'count' => 0],
+        ['name'=>'Атака магией', 'index' => 'attack_magic', 'count' => 0],
+        ['name'=>'Защита от оружия', 'index' => 'defense_magic', 'count' => 0],
+        ['name'=>'Защита от магии', 'index' => 'defense_magic', 'count' => 0],
+    ]
+];
--- index.php
+++ index.php
@@ -6,72 +6,93 @@
     <link rel="stylesheet" href="/style.min.css">
 </head>
 <body>
-
+<?require_once 'config_hero.php'?>
 <div class="game_container">
     <div class="header">
+        <?php $Hero = $arHero['heroInfo']?>
         <img class="hero_img" src="/img/hero.png" alt="">
         <div class="hero_info_block">
-            <div class="hero_nickname">Mys9ys</div>
-            <div class="hero_lvl">lvl: 1</div>
-            <div class="hero_hp">10/10</div>
-            <div class="hero_xp">0</div>
+            <div class="hero_nickname"><?=$Hero['nickname']?></div>
+            <div class="hero_lvl">lvl: <?=$Hero['lvl']?></div>
+            <div class="hero_hp"><?=$Hero['hp']?>/<?=$Hero['hp_now']?></div>
+            <div class="hero_xp"><?=$Hero['xp']?></div>
         </div>
+    </div>
+    <div class="all_information_block">
+        <div class="equipment_block">
+            <div class="equipment_main">
+                <div class="equipment_first">
+                    <div class="equipment_item5 equipment_sword">
+                        <img class="equipment_item_img" src="/img/equip_temp/sword.png" alt="">
+                    </div>
+                    <div class="equipment_item4 equipment_shield">
+                        <img class="equipment_item_img" src="/img/equip_temp/shield.png" alt="">
+                    </div>
+                </div>
+                <div class="equipment_second">
+                    <div class="equipment_item5 equipment_armor">
+                        <img class="equipment_item_img" src="/img/equip_temp/armor.png" alt="">
+                    </div>
+                    <div class="equipment_item4 equipment_pants">
+                        <img class="equipment_item_img" src="/img/equip_temp/pants.png" alt="">
+                    </div>
+                </div>
+                <div class="equipment_third">
+                    <div class="equipment_item3 equipment_helmet">
+                        <img class="equipment_item_img" src="/img/equip_temp/helmet.png" alt="">
+                    </div>
+                    <div class="equipment_item1">
+                        <img class="equipment_item_img" src="/img/equip_temp/gloves.png" alt="">
+                    </div>
+                    <div class="equipment_item1">
+                        <img class="equipment_item_img" src="/img/equip_temp/belt.png" alt="">
+                    </div>
+                    <div class="equipment_item3">
+                        <img class="equipment_item_img" src="/img/equip_temp/boots.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="equipment_jewelry">
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
+                </div>
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
+                </div>
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
+                </div>
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
+                </div>
+                <div class="jewelry_item3">
+                    <img class="jewelry_item_img" src="/img/equip_temp/necklace.png" alt="">
+                </div>
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/bracelet.png" alt="">
+                </div>
+                <div class="jewelry_item1">
+                    <img class="jewelry_item_img" src="/img/equip_temp/bracelet.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="skills_block">
+            <?php foreach ($arHero['heroSkills'] as $skill):?>
+                <div class="skills_position" data-type="<?=$skill['index']?>">
+                    <div class="skills_name"><?=$skill['name']?></div>
+                    <div class="skills_count_box">
+                        <div class="skills_count"><?=$skill['count']?></div><div class="skills_up <?php if($arHero['skill_up']) echo 'skills_up_vis'?>">+</div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+
+
     </div>
 
-    <div class="equipment_block">
-        <div class="equipment_main">
-            <div class="equipment_first">
-                <div class="equipment_item5 equipment_sword">
-                    <img class="equipment_item_img" src="/img/equip_temp/sword.png" alt="">
-                </div>
-                <div class="equipment_item4 equipment_shield">
-                    <img class="equipment_item_img" src="/img/equip_temp/shield.png" alt="">
-                </div>
-            </div>
-            <div class="equipment_second">
-                <div class="equipment_item5 equipment_armor">
-                    <img class="equipment_item_img" src="/img/equip_temp/armor.png" alt="">
-                </div>
-                <div class="equipment_item4 equipment_pants">
-                    <img class="equipment_item_img" src="/img/equip_temp/pants.png" alt="">
-                </div>
-            </div>
-            <div class="equipment_third">
-                <div class="equipment_item3 equipment_helmet">
-                    <img class="equipment_item_img" src="/img/equip_temp/helmet.png" alt="">
-                </div>
-                <div class="equipment_item1">
-                    <img class="equipment_item_img" src="/img/equip_temp/gloves.png" alt="">
-                </div>
-                <div class="equipment_item1">
-                    <img class="equipment_item_img" src="/img/equip_temp/belt.png" alt="">
-                </div>
-                <div class="equipment_item3">
-                    <img class="equipment_item_img" src="/img/equip_temp/boots.png" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="equipment_jewelry">
-            <div class="jewelry_item1">
-                <img class="jewelry_item_img" src="/img/equip_temp/ring.png" alt="">
-            </div>
-            <div class="jewelry_item1"></div>
-            <div class="jewelry_item1"></div>
-            <div class="jewelry_item1"></div>
-            <div class="jewelry_item3">
-                <img class="jewelry_item_img" src="/img/equip_temp/necklace.png" alt="">
-            </div>
-            <div class="jewelry_item1">
-                <img class="jewelry_item_img" src="/img/equip_temp/bracelet.png" alt="">
-            </div>
-            <div class="jewelry_item1"></div>
-        </div>
-    </div>
 
 
 </div>
 </body>
 </html>
-<style>
-
-</style>
