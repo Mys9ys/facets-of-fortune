8e76540e9fe21407e89310c8487ac64eceba8658
mys9ys <rjbexa@yandex.ru>
2022-05-13 17:45:33 +0300

    рюкзак

--- config_hero.php
+++ config_hero.php
@@ -1,6 +1,17 @@
 <?php
 $arHero = [
-
+    'heroInfo' => [
+        'nickname' => 'Mys9ys',
+        'lvl' => 1,
+        'hp'=> 10,
+        'hp_now' => 10,
+        'xp'=> 0
+    ],
+    'money' => [
+        ['name'=> 'Золото', 'index'=>'gold', 'count'=> 5],
+        ['name'=> 'Серебро', 'index'=>'silver', 'count'=> 50],
+        ['name'=> 'Медь', 'index'=>'cuprum', 'count'=> 122],
+    ],
     'heroSkills' => [
         ['name'=>'Сила', 'index' => 'power', 'count' => 0],
         ['name'=>'Магия', 'index' => 'magic', 'count' => 0],
@@ -8,10 +19,10 @@ $arHero = [
     ],
     'skill_up' => 1,
     'heroAttackDefence' => [
-        ['name'=>'Атака оружием', 'index' => 'attack_weapon', 'count' => 0],
-        ['name'=>'Атака магией', 'index' => 'attack_magic', 'count' => 0],
-        ['name'=>'Защита от оружия', 'index' => 'defense_weapon', 'count' => 0],
-        ['name'=>'Защита от магии', 'index' => 'defense_magic', 'count' => 0],
+        ['name'=>'Атака оружием', 'index' => 'attack_weapon', 'min' => 1, 'max' => 3],
+        ['name'=>'Атака магией', 'index' => 'attack_magic', 'min' => 0, 'max' => 0],
+        ['name'=>'Защита от оружия', 'index' => 'defense_weapon', 'min' => 0, 'max' => 0],
+        ['name'=>'Защита от магии', 'index' => 'defense_magic', 'min' => 0, 'max' => 0],
     ],
     'heroProperty' => [
         ['name'=>'Скорость', 'index' => 'defense_magic', 'count' => 0],
@@ -51,4 +62,16 @@ if($arHero['bagCount'] % 10 === 0 ){
 }
 
 $arHero['heroRucksack'] = array_merge($arHero['heroEquipments'], $arHero['heroResources']);
+
+$arSwords['sword_1'] = [
+    'faces' => [
+        ['attack' => 1],
+        ['attack' => 1],
+        ['attack' => 2],
+        ['attack' => 3],
+    ],
+    'min' => 1,
+    'max' => 3
+];
+
 
--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 
 </head>
 <body>
-<?require_once 'config_hero.php'?>
-<?require_once 'template/header.php'?>
+<? require_once 'config_hero.php' ?>
+<? require_once 'template/header.php' ?>
 <div class="game_container">
 
     <div class="all_information_block">
@@ -71,48 +71,49 @@
         </div>
         <div class="skills_att_def_block">
             <div class="skills_block">
-                <?php foreach ($arHero['heroSkills'] as $skill):?>
-                    <div class="skills_position" data-type="<?=$skill['index']?>">
-                        <div class="skills_name"><?=$skill['name']?></div>
+                <?php foreach ($arHero['heroSkills'] as $skill): ?>
+                    <div class="skills_position" data-type="<?= $skill['index'] ?>">
+                        <div class="skills_name"><?= $skill['name'] ?></div>
                         <div class="skills_count_box">
-                            <div class="skills_count"><?=$skill['count']?></div><div class="skills_up <?php if($arHero['skill_up']) echo 'skills_up_vis'?>">+</div>
+                            <div class="skills_count"><?= $skill['count'] ?></div>
+                            <div class="skills_up <?php if ($arHero['skill_up']) echo 'skills_up_vis' ?>">+</div>
                         </div>
                     </div>
-                <?php endforeach;?>
+                <?php endforeach; ?>
             </div>
 
             <div class="attack_defence_desc_block">
-                <?php foreach ($arHero['heroAttackDefence'] as $elem):?>
-                    <div class="add_position" data-type="<?=$elem['index']?>">
-                        <div class="add_name"><?=$elem['name']?></div>
+                <?php foreach ($arHero['heroAttackDefence'] as $elem): ?>
+                    <div class="add_position" data-type="<?= $elem['index'] ?>">
+                        <div class="add_name"><?= $elem['name'] ?></div>
                         <div class="add_count_box">
-                            <div class="add_count"><?=$elem['count']?>-0</div>
+                            <div class="add_count"><?= $elem['min'] . '-' . $elem['max'] ?></div>
                         </div>
                     </div>
-                <?php endforeach;?>
+                <?php endforeach; ?>
             </div>
         </div>
         <div class="property_block">
-            <?php foreach ($arHero['heroProperty'] as $prop):?>
-                <div class="prop_position" data-type="<?=$prop['index']?>">
-                    <div class="prop_name"><?=$prop['name']?></div>
+            <?php foreach ($arHero['heroProperty'] as $prop): ?>
+                <div class="prop_position" data-type="<?= $prop['index'] ?>">
+                    <div class="prop_name"><?= $prop['name'] ?></div>
                     <div class="prop_count_box">
-                        <div class="prop_count"><?=$prop['count']?></div>
+                        <div class="prop_count"><?= $prop['count'] ?></div>
                     </div>
                 </div>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </div>
 
         <div class="craft_block">
-            <?php foreach ($arHero['heroCraft'] as $prop):?>
-                <div class="craft_position" data-type="<?=$prop['index']?>">
-                    <div class="craft_name"><?=$prop['name']?></div>
+            <?php foreach ($arHero['heroCraft'] as $prop): ?>
+                <div class="craft_position" data-type="<?= $prop['index'] ?>">
+                    <div class="craft_name"><?= $prop['name'] ?></div>
                     <div class="craft_count_box">
-                        <div class="craft_lvl"><?=$prop['lvl']?></div>
-                        <div class="craft_count"><?=$prop['count']?></div>
+                        <div class="craft_lvl"><?= $prop['lvl'] ?></div>
+                        <div class="craft_count"><?= $prop['count'] ?></div>
                     </div>
                 </div>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </div>
 
         <div class="rucksack_block">
@@ -123,30 +124,28 @@
                 <div class="rucksack_tab">quest</div>
             </div>
             <div class="rucksack_items">
-                <?php foreach ($arHero['heroRucksack'] as $item):?>
+                <?php foreach ($arHero['heroRucksack'] as $item): ?>
                     <div class="rucksack_item">
-                        <img class="rucksack_item_img" src="<?=$item['img']?>" alt="">
-                        <div class="rucksack_item_count"><?=$item['count']?></div>
+                        <img class="rucksack_item_img" src="<?= $item['img'] ?>" alt="" title="<?= $item['name'] ?>">
+                        <div class="rucksack_item_count"><?= $item['count'] ?></div>
                     </div>
-                <?php endforeach;?>
+                <?php endforeach; ?>
 
-                <?php for ($i = 0; $i < $arHero['bagEmpty']; $i++):?>
-                <div class="rucksack_item">
-                    <img class="rucksack_bag_empty" src="/img/resources/bag.png" alt="">
-                </div>
-                <?php endfor;?>
-                <?php for ($i = 0; $i < $arHero['bagLock'] ; $i++):?>
-                <div class="rucksack_item">
-                    <img class="rucksack_bag_empty" src="/img/resources/bag_lock.png" alt="">
-                </div>
-                <?php endfor;?>
+                <?php for ($i = 0; $i < $arHero['bagEmpty']; $i++): ?>
+                    <div class="rucksack_item">
+                        <img class="rucksack_bag_empty" src="/img/resources/bag.png" alt="">
+                    </div>
+                <?php endfor; ?>
+                <?php for ($i = 0; $i < $arHero['bagLock']; $i++): ?>
+                    <div class="rucksack_item">
+                        <img class="rucksack_bag_empty" src="/img/resources/bag_lock.png" alt="">
+                    </div>
+                <?php endfor; ?>
 
             </div>
 
 
-
         </div>
-
 
 
     </div>
