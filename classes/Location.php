acbee3410f2f8688a769a954d980c40a8decc824
Mys9ys9ka <rjbexa@yandex.ru>
2022-06-30 17:50:02 +0300

    создание карты 1.4

--- classes/Location.php
+++ classes/Location.php
@@ -6,7 +6,7 @@ class Location
 {
     use BaseMethods;
 
-    protected $locInfo;
+    protected $locInfo = [];
     protected $arNpc = [];
 
     protected $fileLoc = '/files/location.json';
@@ -20,6 +20,22 @@ class Location
     protected function loadInfo($id){
         $this->locInfo = $this->loadFile($this->fileLoc)[$id];
         $this->arNpc = $this->loadFile($this->fileNpc)[$id];
+    }
+
+    /**
+     * @return array
+     */
+    public function getArNpc(): array
+    {
+        return $this->arNpc;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLocInfo(): array
+    {
+        return $this->locInfo;
     }
 
 
--- pages/location/index.php
+++ pages/location/index.php
@@ -5,9 +5,22 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/header.php' ?>
 <?php
 parse_str($_SERVER['QUERY_STRING'], $arrQuery);
 $loc = new Location($arrQuery['loc']);
+$locInfo = $loc->getLocInfo();
+$arNpc = $loc->getArNpc()
 ?>
 
 
 <div class="container">
-
+    <div class="location_container">
+        <div class="location_info">
+            <div class="location_name"><?=$locInfo['name']?></div>
+            <div class="location_img">
+                <img src="<?= $locInfo['img']?>" alt="">
+            </div>
+        </div>
+        <div class="location_actions">
+            <?php foreach ($arNpc as $key=>$npc):?>
+            <?php endforeach;?>
+        </div>
+    </div>
 </div>
